99e9b246addd50d5a5fd98048b0f6523a5a85b53
Zhuk Roman <zukroman44@gmail.com>
2023-03-15 18:16:28 +0200

    task 5 in progress

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#name-input");
+const span = document.querySelector("#name-output");
+
+input.addEventListener("submit", inputEvent);
+
+function inputEvent(event) {
+    event.preventDefault()
+
+}
