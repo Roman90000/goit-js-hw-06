3c4aaeba3dc18ae54cacacc85b6dc75e0269daf4
Zhuk Roman <zukroman44@gmail.com>
2023-03-15 13:28:57 +0200

    tas 4 ready

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,13 @@
-const counterValue = 0;
+const counterValue = document.querySelector('#value');
+// counterValue.textContent = Number(0);
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+decrementBtn.addEventListener('click', () => {
+    counterValue.textContent -= 1;
+})
+
+incrementBtn.addEventListener('click', () => {
+    counterValue.textContent = Number(counterValue.textContent) + 1;;
+})
