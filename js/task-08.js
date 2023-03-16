1197ad91bb0c5e00e91acd3c73927834c820e83a
Zhuk Roman <zukroman44@gmail.com>
2023-03-16 19:05:03 +0200

    task 8 ready

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', formSubmit);
 
+function formSubmit(event) {
+    event.preventDefault();
+    const {
+    elements: { email, password }
+    } = event.currentTarget;
+    
+    if (email.value === '' || password.value === '') {
+       alert("Все поля должны быть заполнены!!!")
+    } else {
+        const formData = {
+            email: email.value,
+            password: password.value
+        };
+        event.currentTarget.reset();
+    }
+
+}
