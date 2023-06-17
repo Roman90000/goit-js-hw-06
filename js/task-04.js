437f65384ce0f37fc2623e90606b0feea4d22b87
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2023-06-17 21:07:38 +0300

    Update task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,4 @@
 const counterValue = document.querySelector('#value');
-// counterValue.textContent = Number(0);
 
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
