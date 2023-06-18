e142d0eb463542fff45a161a59ea165cdfcf7e38
Zhuk Roman <95108009+Roman90000@users.noreply.github.com>
2023-06-18 20:05:23 +0300

    task - 6 немного подправил код

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,13 @@
 const input = document.querySelector('#validation-input');
 
-input.addEventListener('blur', () => {
+input.addEventListener('blur', inputBlur);
+
+function inputBlur() {
     if (Number(input.value.length) === Number(input.dataset.length)) {
         input.classList.add("valid");
-        input.classList.replace("invalid", "valid");
-       
+        input.classList.replace("invalid", "valid");       
     } else {
         input.classList.add("invalid");
         input.classList.replace("valid","invalid");
     };
-}
-);
+};
